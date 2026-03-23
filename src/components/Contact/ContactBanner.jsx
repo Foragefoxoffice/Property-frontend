f670724e5ffb88b0be6ff14beb8679b339a5d0b6
Foragefoxoffice <forgefoxoffice@gmail.com>
2026-03-23 12:43:25 +0530

    url

--- src/components/Contact/ContactBanner.jsx
+++ src/components/Contact/ContactBanner.jsx
@@ -12,7 +12,7 @@ export default function ContactBanner({ data }) {
         if (imagePath.startsWith('http') || imagePath.startsWith('data:')) {
             return imagePath;
         }
-        const baseURL = import.meta.env.VITE_API_URL || 'https://dev.placetest.in/api/v1';
+        const baseURL = import.meta.env.VITE_API_URL || 'https://183housingsolutions.com/api/v1';
         const serverURL = baseURL.replace('/api/v1', ''); // Remove /api/v1 suffix if present for static files
         // ensuring slash
         return `${serverURL}${imagePath.startsWith('/') ? '' : '/'}${imagePath}`;
